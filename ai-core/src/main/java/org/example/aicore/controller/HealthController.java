c2636bfe6e116bbae1fe190600e369f0c5f162f2
DmitriyRND <dim3333333333@yandex.ru>
2026-09-09 13:01:47 +0300

    Класс HealthController. Адрес: GET /internal/v1/health. Класс ответа HealthResponse.  Настроил логи: time, level, logger, message.  Добавил подключение базы в модуле rest Core  во всех модулях закоментировал зависимость OpenAi и прописал в проперти отключение OpenAI без этого модули не запускались пока что для проверки

--- ai-core/src/main/java/org/example/aicore/controller/HealthController.java
+++ ai-core/src/main/java/org/example/aicore/controller/HealthController.java
@@ -3,6 +3,8 @@ package org.example.aicore.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.example.aicore.dto.HealthResponse;
+import org.example.aicore.enums.HealthStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
 
     @GetMapping("/health")
-    public HealthResponse health() {
-        log.info("Вызвана проверка работоспособности");
-        return new HealthResponse("UP");
+    public ResponseEntity <HealthResponse> health() {
+        log.debug("Вызвана проверка работоспособности");
+        return  ResponseEntity.ok(new HealthResponse(HealthStatus.UP));
     }
 
 
